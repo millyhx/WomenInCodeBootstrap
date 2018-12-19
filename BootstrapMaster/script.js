bb660cb76ebf66be50609eb9d0d89ffe3db9a534
Milly Huckle <millycyb@outlook.com>
2018-12-19 11:05:53 +0000

    making final changes

--- BootstrapMaster/script.js
+++ BootstrapMaster/script.js
@@ -1,5 +1,6 @@
+//ENABLES THE USER TO SCROLL
 window.onscroll = function() {scrollFunction()};
-
+//INITIATING THE BUTTON THAT ALLOWS THE USER TO SCROLL TO THE TOP OF THE PAGE ONCE THE BUTTON IS CLICKED.
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("myBtn").style.display = "block";
